d9ae7c8a71761f4b01496c79a98a55a508ddd842
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-09-22 08:30:49 -0700

    created the React UI to get access to servers, tip amount, and server name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [servers, setServers] = useState([]);
+  const [serverName, setServerName] = useState('');
+  const [tipAmount, setTipAmount] = useState('');
+
+  useEffect(() => {
+    fetchServers();
+  }, []);
+
+  const fetchServers = async () => {
+    const response = await axios.get('/servers');
+    setServers(response.data);
+  };
+
+  const addServer = async () => {
+    if (serverName) {
+      await axios.post('/add-server', { name: serverName, tips: 0 });
+      setServerName('');
+      fetchServers();
+    }
+  };
+
+  const addTip = async () => {
+    if (tipAmount) {
+      const response = await axios.post('/add-tip', { amount: parseFloat(tipAmount) });
+      setServers(response.data);
+      setTipAmount('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Restaurant Management Platform</h1>
+      <div>
+        <input
+          type="text"
+          value={serverName}
+          onChange={(e) => setServerName(e.target.value)}
+          placeholder="Add Server"
+        />
+        <button onClick={addServer}>Add Server</button>
+      </div>
+      <div>
+        <input
+          type="number"
+          value={tipAmount}
+          onChange={(e) => setTipAmount(e.target.value)}
+          placeholder="Add Tip Amount"
+        />
+        <button onClick={addTip}>Add Tip</button>
+      </div>
+      <div>
+        <h2>Servers</h2>
+        <ul>
+          {servers.map((server) => (
+            <li key={server.name}>{`${server.name}: ${server.tips.toFixed(2)}`}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
